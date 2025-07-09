55ff31326b46b164a373c88e7f9bb7355390ff63
Gobinda Das <dasgobinda9955@gmail.com>
2025-07-09 09:12:04 +0530

    feat(types): define UserSignUpData, UserProfile, and SidebarItem interfaces

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+import { LucideIcon } from "lucide-react";
+
+
+
+export interface UserSignUpData {
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    password: string;
+}
+
+
+export interface UserProfile {
+    name: string;
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    avatar: string;
+}
+
+
+export interface SidebarItem {
+    id: string;
+    href: string;
+    label: string;
+    icon: LucideIcon;
+}
